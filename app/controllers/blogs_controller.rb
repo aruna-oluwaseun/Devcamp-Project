8d406a58f9a4c01341ebc8fb0d740501a7b56841
aruna seun <arunaseun@yahoo.com>
2018-07-02 00:03:46 +0100

    Data validation

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
 
   # GET /blogs
   # GET /blogs.json
@@ -23,6 +23,15 @@ class BlogsController < ApplicationController
 
   # POST /blogs
   # POST /blogs.json
+
+  def toggle_status
+  if @blog.draft?
+    @blog.published!
+   elsif @blog.published?
+    @blog.draft!
+    end
+    redirect_to blogs_url, notice: 'post status updated.'
+end
   def create
     @blog = Blog.new(blog_params)
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
   get 'contact-us',to: 'pages#contact'
   resources :news
   resources :blogs
+  resources :blogs do
+    member do
+    get:toggle_status
+
+  end
+end
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
