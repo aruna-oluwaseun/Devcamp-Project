67b69961bc826a2bdc2f38df8bc9655ab1a6449a
aruna seun <arunaseun@yahoo.com>
2018-07-02 16:35:35 +0100

    Integrated Database relationships between topic and blogpost

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_24_172946) do
+ActiveRecord::Schema.define(version: 2018_07_02_150652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,9 @@ ActiveRecord::Schema.define(version: 2018_06_24_172946) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.integer "status", default: 0
+    t.bigint "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
+    t.index ["topic_id"], name: "index_blogs_on_topic_id"
   end
 
   create_table "friendly_id_slugs", id: :serial, force: :cascade do |t|
@@ -60,6 +62,12 @@ ActiveRecord::Schema.define(version: 2018_06_24_172946) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "slug"
@@ -68,4 +76,5 @@ ActiveRecord::Schema.define(version: 2018_06_24_172946) do
     t.index ["slug"], name: "index_users_on_slug", unique: true
   end
 
+  add_foreign_key "blogs", "topics"
 end
