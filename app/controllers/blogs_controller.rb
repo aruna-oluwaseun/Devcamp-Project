37aa2738a43e2da77974d5cbb711bd3ef4f14272
aruna seun <arunaseun@yahoo.com>
2018-08-08 12:53:36 +0100

    Completed the UI for all the pages

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,18 +1,21 @@
+
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
-layout 'blog'
-access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+  layout "blog"
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
+
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.all
+    @blogs = Blog.page(params[:page]).per(5)
+    @page_title = "My Portfolio Blog"
   end
 
   # GET /blogs/1
   # GET /blogs/1.json
   def show
     @page_title = @blog.title
-    @seo_keywords = @blog.title
+    @seo_keywords = @blog.body
   end
 
   # GET /blogs/new
@@ -26,25 +29,14 @@ access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :
 
   # POST /blogs
   # POST /blogs.json
-
-  def toggle_status
-  if @blog.draft?
-    @blog.published!
-   elsif @blog.published?
-    @blog.draft!
-    end
-    redirect_to blogs_url, notice: 'post status updated.'
-end
   def create
     @blog = Blog.new(blog_params)
 
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
-        format.json { render :show, status: :created, location: @blog }
+        format.html { redirect_to @blog, notice: 'Your post is now live.' }
       else
         format.html { render :new }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -55,10 +47,8 @@ end
     respond_to do |format|
       if @blog.update(blog_params)
         format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
-        format.json { render :show, status: :ok, location: @blog }
       else
         format.html { render :edit }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -68,9 +58,19 @@ end
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      format.html { redirect_to blogs_url, notice: 'Post was removed.' }
       format.json { head :no_content }
     end
+  end
+
+  def toggle_status
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+      @blog.draft!
+    end
+        
+    redirect_to blogs_url, notice: 'Post status has been updated.'
   end
 
   private
@@ -81,6 +81,6 @@ end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title)
+      params.require(:blog).permit(:title, :body)
     end
 end
